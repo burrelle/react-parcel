c6d31199352eee1fbd4d87b644980d17e6d69ef7
Evan Burrell <burrelle@oregonstate.edu>
2018-07-12 15:31:14 -0700

    Homepage for navigation purpose

---
+++ src/components/home.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Home = () => {
+    return (
+        <div className="mt-32 flex items-center justify-center">
+            <div className="max-w-lg">
+                This is the homepage. 
+            </div>
+        </div>
+    )
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,18 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/navbar.jsx";
-import Sidebar from "./components/sidebar.jsx";
 import Content from "./components/content.jsx";
+import Home from "./components/home.jsx";
 
 const App = () => {
   return (
     <Router>
       <div>
         <Navbar />
-      <Route path="/docs" component={Content}>
-      </Route>
-      {/* <Route path="/faq" componet={} />       */}
-      {/* <Route path="/" componet={} /> */}
-      </div>
+          <Route path="/docs" component={Content} />
+          {/* <Route path="/faq" componet={} />       */}
+          <Route exact path="/" component={Home} />
+        </div>
     </Router>
   );
 };
